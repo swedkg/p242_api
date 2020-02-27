13f14af27a6be19d52ecfbb456957b5a0cf7f002
Dimitrios Giannopoulos <dimitrios.giannopoulos@gmail.com>
2020-02-27 11:57:14 +0100

    ActionCable working

--- app/channels/web_notifications_channel.rb
+++ app/channels/web_notifications_channel.rb
@@ -1,16 +1,30 @@
-class WebNotificationsChannel < ApplicationCable::Channel
-  # def subscribed
-  # puts('subscribed')
-  #   ActionCable.server.broadcast 'web_notifications_channel', message: 'subscribed'
-  #   # stream_from "web_notifications_channel"
-  # end
+# class WebNotificationsChannel < ApplicationCable::Channel
+#   def subscribed
+#   puts('subscribed')
+#     stream_from "web_notifications_channel"
+#   end
   
-  # def received(data)
-  #   puts(data)
-  #   ActionCable.server.broadcast 'web_notifications_channel', message: data
-  # end
+#   def received(data)
+#     puts(data)
+#     ActionCable.server.broadcast 'web_notifications_channel', message: data
+#   end
 
-  # def unsubscribed
-  #   # Any cleanup needed when channel is unsubscribed
-  # end
+#   def unsubscribed
+#     # Any cleanup needed when channel is unsubscribed
+#   end
+# end
+
+class WebNotificationsChannel < ApplicationCable::Channel
+  def subscribed
+  puts("helloz")
+    # stream_from "some_channel"
+    stream_from "web_notifications_channel"
+
+  end
+
+  def unsubscribed
+    # Any cleanup needed when channel is unsubscribed
+  end
 end
+
+
